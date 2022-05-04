0acd3fedb9972996d0c931e97406198fccc1fa0f
Paskuvan <majopaskuvan@gmail.com>
2022-05-04 14:27:41 -0400

    second commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
+import { MoralisProvider } from 'react-moralis'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider 
+    const serverUrl={process.env.NEXT_PUBLIC_MORALIS_SERVER}
+    const appId={process.env.NEXT_PUBLIC_MORALIS_APP_ID}
+  >
+    <Component {...pageProps} />
+
+  </MoralisProvider>
+  )
+  
 }
 
 export default MyApp
