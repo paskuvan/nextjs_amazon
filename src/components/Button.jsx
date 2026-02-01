4ef9016a272abedbe2ac0cb3460c2d19d3fc4903
paskuvan <majopaskuvan@gmail.com>
2026-02-01 17:19:44 -0300

    Implement Courses

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function Button({ label, primary }) {
+export default function Button({ label, classes,primary  }) {
   return (
   <button 
   className={`shadow-util px-6 p-2.5 rounded-full font-medium transition-all hover:bg-neutral-100 focus:bg-neutral-100 
-    ${primary ? "bg-lime-200 hover:bg-line-400 focus:bg-lime-400" : "hover:bg-neutral-100 focus:bg-neutral-100"}`}>
+  ${classes}  ${primary ? "bg-lime-200 hover:bg-line-400 focus:bg-lime-400" : "hover:bg-neutral-100 focus:bg-neutral-100"}`}>
       {label}
     </button>
   )
