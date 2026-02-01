5ed031db3b3f4498eaa10b6970fe55608778c81d
paskuvan <majopaskuvan@gmail.com>
2026-02-01 16:22:11 -0300

    Implement Course Categories

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import Hero from './components/Hero.jsx';
 import BrandBanner from './components/BrandBanner.jsx';
+import CourseCategories from './components/CourseCategories.jsx';
 
 export default function App () {
   return <>
@@ -9,6 +10,7 @@ export default function App () {
   <main>
     <Hero />
     <BrandBanner/>
+    <CourseCategories/>
   </main>
   <Footer />
   </>;
